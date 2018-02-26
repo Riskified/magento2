d2e5766f151ed9e1137cc46aa601153b465cbd92
patriknilsson <patrik.nilsson@key-component.com>
2018-02-26 13:34:02 +0100

    Added composer.json and registration.php

---
+++ registration.php
@@ -0,0 +1,4 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Riskified_Decider', __DIR__ . '/app/code/Riskified/Decider');
